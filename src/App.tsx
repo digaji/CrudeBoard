6cf591df15a13ffa8833063fd6f2918fac16deba
Jason J. W <71363607+digaji@users.noreply.github.com>
2022-04-05 01:51:31 +0700

    Updated to latest commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export default function App():JSX.Element {
   return (
     <div className="text-center">
       <header className="bg-[#282c34] min-h-screen flex flex-col items-center justify-center text-white text-3xl">
@@ -22,5 +22,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
