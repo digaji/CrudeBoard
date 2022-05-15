9917d0f62b1f4330c1ff3708d1837dd8c525c9ee
Jason J. W <71363607+digaji@users.noreply.github.com>
2022-05-16 00:39:25 +0700

    Implemented 404 error handling

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Navbar from "./common/Navbar";
 import Home from "./pages/Home";
 import SignIn from "./pages/SignIn";
+import NotFound from "./pages/NotFound";
 import "./App.css";
 
 export default function App(): JSX.Element {
@@ -18,6 +19,12 @@ export default function App(): JSX.Element {
           <Navbar />
           <SignIn />
         </div>
+      } />
+      <Route path="*" element={
+      <div className="min-h-screen bg-gradient-to-b from-crude-blue to-crude-darkBlue">
+        <Navbar />
+        <NotFound />
+      </div>
       } />
     </Routes>
   );
---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,10 @@
+export default function NotFound(): JSX.Element {
+  return (
+    <div>
+      <div className="flex flex-col items-center justify-center text-white font-medium">
+        <h1 className="text-6xl my-10">404 Error</h1>
+        <h2 className="text-3xl">The page you're looking for is not found :(</h2>
+      </div>
+    </div>
+  );
+}
