0b1fda19eaf5ea16ca9af8ab720e240d435247b0
Jason J. W <71363607+digaji@users.noreply.github.com>
2022-05-23 21:53:16 +0700

    Added experimental delete button

--- src/common/Task.tsx
+++ src/common/Task.tsx
@@ -2,19 +2,25 @@ import React from "react";
 import { Draggable } from "react-beautiful-dnd";
 
 export default class Task extends React.Component<any> {
+
+  handleDelete() {
+    console.log("Testing");
+  }
+
   render() {
     return (
-      <Draggable draggableId={this.props.task.id} index={this.props.index}>
+      <Draggable draggableId={this.props.task.id} index={this.props.index} isDragDisabled={false}>
         {(provided, snapshot) => (  // Snapshot used to style Draggables
           // Droppable expects child to be a function that returns React.component
           // Task
           <div
-            className={`mb-2 rounded-sm border-2 border-gray-200 p-2 ${snapshot.isDragging ? "bg-white border-black" : "bg-white"}`}
+            className={`mb-2 rounded-sm border-2 border-gray-200 p-2 group bg-white ${snapshot.isDragging ? "border-black" : ""}`}
             {...provided.draggableProps}
             {...provided.dragHandleProps}
             ref={provided.innerRef}
           >
             {this.props.task.content}
+            <button className="float-right opacity-0 transition-all duration-200 hover:bg-red-300 group-hover:opacity-100" onClick={this.handleDelete}>X</button>
           </div>
         )}
       </Draggable>
