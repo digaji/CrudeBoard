948f7010d92705dad7279a48760611accf0edb2c
Jason J. W <71363607+digaji@users.noreply.github.com>
2022-05-25 23:02:06 +0700

    Implemented mock login and register logic

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,13 +1,46 @@
 import InputBox from "../common/InputBox";
+import { useState } from "react";
+import axios from "axios";
 
 export default function Register(): JSX.Element {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const url: string = "http://localhost:3000/api/v1/auth/register";
+
   return (
     <div className="mt-[5rem]">
-      <form action="" className="mx-auto grid max-w-md grid-cols-1 gap-6 rounded-md border-2 border-slate-100">
+      <form
+        onSubmit={async (e) => {
+          e.preventDefault();
+          console.log(email, password);
+          const res = await axios.post(url, {
+            username: email,
+            password: password,
+          });
+          console.log(res);
+        }}
+        className="mx-auto grid max-w-md grid-cols-1 gap-6 rounded-md border-2 border-slate-100"
+      >
         <h1 className="my-5 text-center text-2xl font-medium text-white">Create An Account</h1>
-        <InputBox type="text" name="email" id="email" placeholder="Email Address" />
-        <InputBox type="password" name="password" id="password" placeholder="Password" />
-        <InputBox type="password" name="confirm-password" id="confirm-password" placeholder="Confirm Password" />
+        <InputBox
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+          type="text"
+          name="email"
+          id="email"
+          placeholder="Email Address"
+        />
+        <InputBox
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+          type="password"
+          name="password"
+          id="password"
+          placeholder="Password"
+        />
+        <InputBox onChange={(e) => {}} type="password" name="confirm-password" id="confirm-password" placeholder="Confirm Password" />
         <input
           type="submit"
           value="Register"
--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,12 +1,45 @@
 import InputBox from "../common/InputBox";
+import { useState } from "react";
+import axios from "axios";
 
 export default function SignIn(): JSX.Element {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const url: string = "http://localhost:3000/api/v1/auth/login";
+
   return (
     <div className="mt-[5rem]">
-      <form action="" className="mx-auto grid max-w-md grid-cols-1 gap-6 rounded-md border-2 border-slate-100">
+      <form
+        onSubmit={async (e) => {
+          e.preventDefault();
+          console.log(email, password);
+          const res = await axios.post(url, {
+            username: email,
+            password: password,
+          });
+          console.log(res);
+        }}
+        className="mx-auto grid max-w-md grid-cols-1 gap-6 rounded-md border-2 border-slate-100"
+      >
         <h1 className="my-5 text-center text-2xl font-medium text-white">Sign In</h1>
-        <InputBox type="text" name="email" id="email" placeholder="Email Address" />
-        <InputBox type="password" name="password" id="password" placeholder="Password" />
+        <InputBox
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+          type="text"
+          name="email"
+          id="email"
+          placeholder="Email Address"
+        />
+        <InputBox
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+          type="password"
+          name="password"
+          id="password"
+          placeholder="Password"
+        />
         <input
           type="submit"
           value="Sign In"
