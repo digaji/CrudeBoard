a77ff491f144c89efb23b73855fb1e78cd56764d
Jason J. W <71363607+digaji@users.noreply.github.com>
2022-05-30 01:44:47 +0700

    Changed backendUrl to deployment url

--- src/misc/Constants.ts
+++ src/misc/Constants.ts
@@ -1,1 +1,2 @@
-export const backendUrl = "http://localhost:3000/api/v1";
+export const backendUrl = "https://crudeboard-backend-jidxl75r2a-et.a.run.app/api/v1";
+// export const backendUrl = "http://localhost:3000/api/v1";
