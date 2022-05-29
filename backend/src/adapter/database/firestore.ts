25c63453c00e3b3204ad7304da6271013c1e70ce
Jason J. W <71363607+digaji@users.noreply.github.com>
2022-05-30 00:44:40 +0700

    Changed how serviceaccountkey is handled

--- backend/src/adapter/database/firestore.ts
+++ backend/src/adapter/database/firestore.ts
@@ -2,11 +2,17 @@ import { initializeApp, applicationDefault, cert } from "firebase-admin/app";
 import { getFirestore, Timestamp, FieldValue } from "firebase-admin/firestore";
 import { getAuth } from "firebase-admin/auth";
 
-const serviceAccountKey = require("./serviceAccountKey.json");
+if (process.env.NODE_ENV === "production") {
+    initializeApp({
+        credential: applicationDefault()
+    })
+} else {
+    const serviceAccountKey = require("./serviceAccountKey.json");
 
-initializeApp({
-    credential: cert(serviceAccountKey),
-});
+    initializeApp({
+        credential: cert(serviceAccountKey),
+    });
+}
 
 export const auth = getAuth();
 export const db = getFirestore();
